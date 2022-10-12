c778eac76176e53d21fc546da65a3c0f1af551b6
Manali-Seth <sethmanali10@gmail.com>
2022-10-12 11:14:45 -0700

    Connected to MongoDB

--- applications/server/app.js
+++ applications/server/app.js
@@ -9,7 +9,19 @@ require('dotenv').config();
 const app = express();
 
 //db
-
+const db = mongoose.connect(
+    `mongodb://admin:csc890team4@50.18.130.129:27017/csc890Team4`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    //   useFindAndModify: false,
+    },
+    (err) => {
+      if (err) return console.error(err);
+      console.log("connected to mongoDB");
+    }
+  );
+  
 
 //middleware
 
@@ -20,7 +32,7 @@ app.use(cors({origin:true, credentials:true}));
 
 
 //port 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8001;
 
 //listener
 const server = app.listen(port, ()=> console.log(`Server is running on port ${port}`))
