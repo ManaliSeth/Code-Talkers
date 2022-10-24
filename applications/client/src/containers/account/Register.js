bde7175fbf63ed6b983503c374aec92f84c731dc
snehalp31996 <snehalp31996@gmail.com>
2022-10-24 14:28:05 -0700

    CSC 890 format code

--- applications/client/src/containers/account/Register.js
+++ applications/client/src/containers/account/Register.js
@@ -1,111 +1,111 @@
 import { useState } from "react";
 import React from "react";
 import Image from "react-bootstrap/Image";
-import accountImg from '../../assets/Account/account.svg'
+import accountImg from "../../assets/Account/account.svg";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import "./account.css";
 
+const Register = () => {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
-  const Register = () => {
-    const[data, setData] = useState({
-      firstName:"",
-      lastName:"",
-      email:"",
-      password:""
-    });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
 
-    const [error, setError] = useState("");
-    const navigate = useNavigate();
-    const handleChange = ({currentTarget: input}) =>{
-      setData({...data,[input.name]:input.value});
-    };
-
-    const handleSubmit = async(e) =>{
-      e.preventDefault();
-      try{
-        const url = "http://localhost:8080/api/users";
-        const {data:res} = await axios.post(url,data);
-        navigate("/login")
-        console.log(res.message);
-      }catch(error){
-        if(error.response && 
-          error.response.status >= 400 && 
-          error.response.status <= 500){
-            setError(error.response.data.message)
-          }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:8080/api/users";
+      const { data: res } = await axios.post(url, data);
+      navigate("/login");
+      console.log(res.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
       }
     }
-    return (
-      <div className="base-container">
-        <span className="box square border border-dark">
-        
-          <div className="header">Register</div>
-          <div className="content">
-            <div className="image">
-              <Image src={accountImg} alt="Register" height="100%" width="100%" />
-            </div>
+  };
+  return (
+    <div className="base-container">
+      <span className="box square border border-dark">
+        <div className="header">Register</div>
+        <div className="content">
+          <div className="image">
+            <Image src={accountImg} alt="Register" height="100%" width="100%" />
+          </div>
 
-            <div className="form-class">              
-              <form className="form-group" onSubmit={handleSubmit}>
-                <input 
-                  type="text" 
-                  name="firstName" 
-                  placeholder="First Name" 
-                  onChange={handleChange}
-                  value={data.firstName}
-                  required
-                  className="input"
-                  />
-                  <input 
-                  type="text" 
-                  name="lastName" 
-                  placeholder="Last Name" 
-                  onChange={handleChange}
-                  value={data.lastName}
-                  required
-                  className="input"
-                  />
-                  <input 
-                  type="email" 
-                  name="email" 
-                  placeholder="Email" 
-                  onChange={handleChange}
-                  value={data.email}
-                  required
-                  className="input"
-                  />
-                  <input 
-                  type="password" 
-                  name="password" 
-                  placeholder="Password" 
-                  onChange={handleChange}
-                  value={data.password}
-                  required
-                  className="input"
-                  />
-                  {/* <Form.Select aria-label="Default select example">
+          <div className="form-class">
+            <form className="form-group" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="First Name"
+                onChange={handleChange}
+                value={data.firstName}
+                required
+                className="input"
+              />
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+                onChange={handleChange}
+                value={data.lastName}
+                required
+                className="input"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={handleChange}
+                value={data.email}
+                required
+                className="input"
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                value={data.password}
+                required
+                className="input"
+              />
+              {/* <Form.Select aria-label="Default select example">
         <option>Open this select menu</option>
       <option value="1">One</option>
       <option value="2">Two</option>
       <option value="3">Three</option>
     </Form.Select> */}
-                  {error && <div className="error_message">{error}</div>}
-                  <button type="submit" className="btn highlighted-btn">
-                    Register
-                  </button>
-                </form>
-            </div>
-
-          </div> 
-          <div className="footer">
-            <p id="signUp"> Already registered? <Link to="/Login"> Sign in </Link></p>
-
+              {error && <div className="error_message">{error}</div>}
+              <button type="submit" className="btn highlighted-btn">
+                Register
+              </button>
+            </form>
           </div>
-        </span>
-      </div>
-    );
-  }
-
+        </div>
+        <div className="footer">
+          <p id="signUp">
+            {" "}
+            Already registered? <Link to="/Login"> Sign in </Link>
+          </p>
+        </div>
+      </span>
+    </div>
+  );
+};
 
 export default Register;
--- applications/client/src/containers/home/Home.js
+++ applications/client/src/containers/home/Home.js
@@ -10,55 +10,71 @@ import { Col, Row } from "react-bootstrap";
 import "../../index.css";
 
 class Home extends Component {
-  render(){
+  render() {
     return (
-      <div className="div-container" >
-        <br/>
-            <div className="div-containerrow">
-              <h1> CSC 890 Graduate Seminar Project</h1>
-              <p> Start by picking any of the use-cases below to start generating AI content.</p>
+      <div className="div-container">
+        <br />
+        <div className="div-containerrow">
+          <h1> CSC 890 Graduate Seminar Project</h1>
+          <p>
+            {" "}
+            Start by picking any of the use-cases below to start generating AI
+            content.
+          </p>
+        </div>
+        <br />
+        <Row>
+          <Col>
+            <div className="icon-picture">
+              <Image
+                src={image1}
+                alt="Image1"
+                className="profile-picture-adjust"
+              ></Image>
             </div>
-            <br />
-            <Row>
-              <Col>
-              <div className="icon-picture">
-                <Image src={image1} alt="Image1" className="profile-picture-adjust"></Image>
-              </div>  
-              <Card
-                header="Code To Text"
-                title="Translate Code to Natural Language"
-                text="Explain a complicated piece of code. User can input the code in the text box."
-                theLink="/codeToText" />
-              </Col>
-              <Col>
-                <div className="icon-picture">
-                  <Image src={image2} alt="Image2" className="profile-picture-adjust"></Image>
-                </div>             
-              <Card
-                header="Text To Code"
-                title="Generate Code"
-                text="Generates code in any language mentioned by the user. The user has to select the language type."
-                theLink="/textToCode" />
-              </Col>
-              <Col>
-              <div className="icon-picture">
-                  <Image src={image3} alt="Image3" className="profile-picture-adjust"></Image>
-                </div>
-              <Card
-                header="Code To Code"
-                title="Translate programming languages"
-                text="Translate from one programming language to another, use the comments to specify the source and target languages."
-                theLink="/codeToCode" />
-              </Col>
-            </Row>
-            <br />
-            <br />
+            <Card
+              header="Code To Text"
+              title="Translate Code to Natural Language"
+              text="Explain a complicated piece of code. User can input the code in the text box."
+              theLink="/codeToText"
+            />
+          </Col>
+          <Col>
+            <div className="icon-picture">
+              <Image
+                src={image2}
+                alt="Image2"
+                className="profile-picture-adjust"
+              ></Image>
+            </div>
+            <Card
+              header="Text To Code"
+              title="Generate Code"
+              text="Generates code in any language mentioned by the user. The user has to select the language type."
+              theLink="/textToCode"
+            />
+          </Col>
+          <Col>
+            <div className="icon-picture">
+              <Image
+                src={image3}
+                alt="Image3"
+                className="profile-picture-adjust"
+              ></Image>
+            </div>
+            <Card
+              header="Code To Code"
+              title="Translate programming languages"
+              text="Translate from one programming language to another, use the comments to specify the source and target languages."
+              theLink="/codeToCode"
+            />
+          </Col>
+        </Row>
+        <br />
+        <br />
       </div>
-      
-);
-
+    );
   }
-  
 }
 
 export default Home;
