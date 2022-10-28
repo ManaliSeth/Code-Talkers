5198693b6961184d23b5f392c6d7b71848d7b060
snehalp31996 <snehalp31996@gmail.com>
2022-10-27 18:05:20 -0700

    CSC890 added jwt token and cookie expiration on server side

--- applications/server/models/user.js
+++ applications/server/models/user.js
@@ -9,13 +9,25 @@ const userSchema = new mongoose.Schema({
   userType: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
-userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
-    expiresIn: "7d",
-  });
-  return token;
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //user model
