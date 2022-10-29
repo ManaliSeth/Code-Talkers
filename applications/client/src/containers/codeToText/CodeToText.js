21e81136e754b9acb8499bfd83f7d6ab7bf3a614
snehalp31996 <snehalp31996@gmail.com>
2022-10-29 12:42:41 -0700

    CSC890 added error page, Dropdown for usertype, codeTOText page

--- applications/client/src/containers/codeToText/CodeToText.js
+++ applications/client/src/containers/codeToText/CodeToText.js
@@ -1,19 +1,53 @@
-import React from "react";
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Form, Button, Card } from "react-bootstrap";
 import { Col, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import "../../index.css";
 const { Configuration, OpenAIApi } = require("openai");
-class CodeToText extends Component {
-  constructor() {
-    super();
-    this.state = {
-      heading: "The response from the AI will be shown here",
-      response: "....... await the response",
-    };
-  }
 
-  onFormSubmit = (e) => {
+const CodeToText = () => {
+  // const callCodeToText = async () => {
+  //   try {
+  //     const res = await fetch("http://localhost:3000/codeToText");
+  //     console.log(res);
+  //   } catch (error) {
+  //     console.log(error);
+  //     // navigate("/login");
+  //   }
+  // };
+  // // const navigate = useNavigate();
+  // // const callCodeToText = async () => {
+  // //   console.log("/codeToText");
+  // //   try {
+  // //     const res = await fetch("/codeToText", {
+  // //       method: "GET",
+  // //       headers: {
+  // //         Accept: "appllication/json",
+  // //         "Content-Type": "application/json",
+  // //       },
+  // //       credentials: "include",
+  // //     });
+  // //     console.log(res);
+  // //     const data = await res.json();
+  // //     console.log(data);
+
+  // //     if (!res.status === 200) {
+  // //       const error = new Error(res.error);
+  // //       throw error;
+  // //     }
+  // //   } catch (error) {
+  // //     console.log(error);
+  // //     navigate("/login");
+  // //   }
+  // // };
+
+  // useEffect(() => {
+  //   callCodeToText();
+  // }, []);
+
+  const [response, setResponse] = useState("....... await the response");
+
+  const onFormSubmit = (e) => {
     e.preventDefault();
 
     const formData = new FormData(e.target),
@@ -24,8 +58,7 @@ class CodeToText extends Component {
     //OPENAI
 
     const configuration = new Configuration({
-      apiKey: "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",
-      // apiKey: process.env.OPENAI_API_KEY,
+      apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
 
@@ -41,101 +74,93 @@ class CodeToText extends Component {
         stop: ['"""'],
       })
       .then((response) => {
-        this.setState({
-          heading: `AI Code Explanation :`,
-          response: `${response.data.choices[0].text}`,
-        });
+        setResponse(response.data.choices[0].text);
       });
   };
 
-  render() {
-    return (
-      <Container>
-        <br />
-        <div className="div-containerrow">
-          <h1> Generate Explanation for your code</h1>
-          <h4> Enter code and display the result for it.</h4>
-        </div>
-        <br />
-        <Row>
-          <Col>
-            <Form onSubmit={this.onFormSubmit}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label> What Code you want to undertsand?</Form.Label>
+  return (
+    <Container>
+      <br />
+      <div className="div-containerrow">
+        <h1> Generate Explanation for your code</h1>
+        <h4> Enter code and display the result for it.</h4>
+      </div>
+      <br />
+      <Row>
+        <Col>
+          <Form method="POST" onSubmit={onFormSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label> What Code you want to undertsand?</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="productName"
+                placeholder="Enter Code"
+                className="feedback_form_question"
+                rows={5}
+              />
+              <Form.Text className="text-muted">
+                Enter as much information as possible for more accurate
+                description.
+              </Form.Text>
+            </Form.Group>
+            <Button variant="primary" size="lg" type="submit">
+              Get AI Suggestions
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      <br />
+      <br />
+      <Card>
+        <Card.Body>
+          <br />
+          <Card.Text>
+            <h4>{response}</h4>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+
+      <h1>User Feedback</h1>
+      <Row>
+        <div className="mb-3">
+          <div className="form">
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="email"
+                  placeholder="Your Email"
+                  id="feedback_form_email"
+                  className="feedback_form_email"
+                />
+                <Form.Label>Enter the output generated</Form.Label>
                 <Form.Control
                   as="textarea"
-                  name="productName"
-                  placeholder="Enter Code"
-                  className="feedback_form_question"
+                  name="feedback_form_answer"
+                  placeholder="Answer Generated"
+                  className="feedback_form_answer"
                   rows={5}
+                  required="true"
                 />
-                <Form.Text className="text-muted">
-                  Enter as much information as possible for more accurate
-                  description.
-                </Form.Text>
+                <Form.Label>Enter your feedback</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  name="feedback_form_feedback"
+                  placeholder="Answer Generated"
+                  className="feedback_form_feedback"
+                  rows={5}
+                  required="true"
+                />
               </Form.Group>
               <Button variant="primary" size="lg" type="submit">
-                Get AI Suggestions
+                Submit Feedback
               </Button>
             </Form>
-          </Col>
-        </Row>
-        <br />
-        <br />
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <h1>{this.state.heading}</h1>
-            </Card.Title>
-            <br />
-            <Card.Text>
-              <h4>{this.state.response}</h4>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <br />
-
-        <h1>User Feedback</h1>
-        <Row>
-          <div className="mb-3">
-            <div className="form">
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control
-                    type="email"
-                    placeholder="Your Email"
-                    id="feedback_form_email"
-                    className="feedback_form_email"
-                  />
-                  <Form.Label>Enter the output generated</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    name="feedback_form_answer"
-                    placeholder="Answer Generated"
-                    className="feedback_form_answer"
-                    rows={5}
-                    required="true"
-                  />
-                  <Form.Label>Enter your feedback</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    name="feedback_form_feedback"
-                    placeholder="Answer Generated"
-                    className="feedback_form_feedback"
-                    rows={5}
-                    required="true"
-                  />
-                </Form.Group>
-                <Button variant="primary" size="lg" type="submit">
-                  Submit Feedback
-                </Button>
-              </Form>
-            </div>
           </div>
-        </Row>
-      </Container>
-    );
-  }
-}
+        </div>
+      </Row>
+    </Container>
+  );
+};
 
 export default CodeToText;
